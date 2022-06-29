7e808b54cef0691aeab5575fc8e9274076586a50
elysian <tsujianii@tuta.io>
2022-06-29 16:03:27 +0800

    encoding

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use core::ops;
+use pancake::Vec;
 
 /// An instruction.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -11,7 +12,54 @@ pub enum Inst {
 }
 
 impl Inst {
+    #[inline]
+    fn from_bytes(bytes: &[u8]) -> Option<Inst> {
+        let inst = match bytes {
+            [0x48, 0x8D, 0x0D, a, b, c, d, ..] => {
+                Inst::Lea(Reg::Rcx, i32::from_le_bytes([*a, *b, *c, *d]))
+            }
+            [0xFF, 0x15, a, b, c, d, ..] => Inst::Call(i32::from_le_bytes([*a, *b, *c, *d])),
+            [0x50, ..] => Inst::Push(Reg::Rax),
+            [0x58, ..] => Inst::Pop(Reg::Rax),
+            [0xC3, ..] => Inst::Ret,
+            _ => return None,
+        };
+
+        Some(inst)
+    }
+
+    #[inline]
+    pub fn to_bytes(&self) -> Vec<u8, 15> {
+        let mut bytes = Vec::new();
+
+        unsafe {
+            match self {
+                Inst::Call(rel) => {
+                    bytes.extend_from_slice_unchecked(&[0xFF, 0x15]);
+                    bytes.extend_from_slice_unchecked(&rel.to_le_bytes());
+                }
+                Inst::Lea(Reg::Rcx, rel) => {
+                    bytes.extend_from_slice_unchecked(&[0x48, 0x8D, 0x0D]);
+                    bytes.extend_from_slice_unchecked(&rel.to_le_bytes());
+                }
+                Inst::Pop(Reg::Rax) => {
+                    bytes.push_unchecked(0x58);
+                }
+                Inst::Push(_) => {
+                    bytes.push_unchecked(0x50);
+                }
+                Inst::Ret => {
+                    bytes.push_unchecked(0xC3);
+                }
+                _ => unreachable!(),
+            }
+        }
+
+        bytes
+    }
+
     /// obtains the length of the instruction (max 15)
+    #[inline]
     pub fn len(&self) -> usize {
         match self {
             Inst::Call(_) => 6,
@@ -28,6 +76,7 @@ impl Inst {
 pub enum Reg {
     Rax,
     Rcx,
+    Rdx,
 }
 
 #[derive(Clone, Copy, Eq, PartialEq)]
@@ -76,7 +125,7 @@ impl<'a> Iterator for InstIter<'a> {
     fn next(&mut self) -> Option<WithIp> {
         let rest = &self.bytes[self.offset..];
 
-        match try_parse(rest) {
+        match Inst::from_bytes(rest) {
             Some(inst) => {
                 let ip = self.ip + self.offset;
 
@@ -89,22 +138,6 @@ impl<'a> Iterator for InstIter<'a> {
     }
 }
 
-#[inline]
-fn try_parse(bytes: &[u8]) -> Option<Inst> {
-    let inst = match bytes {
-        [0x48, 0x8D, 0x0D, a, b, c, d, ..] => {
-            Inst::Lea(Reg::Rcx, i32::from_le_bytes([*a, *b, *c, *d]))
-        }
-        [0xFF, 0x15, a, b, c, d, ..] => Inst::Call(i32::from_le_bytes([*a, *b, *c, *d])),
-        [0x50, ..] => Inst::Push(Reg::Rax),
-        [0x58, ..] => Inst::Pop(Reg::Rax),
-        [0xC3, ..] => Inst::Ret,
-        _ => return None,
-    };
-
-    Some(inst)
-}
-
 fn main() {
     let insts = InstIter::from_bytes(
         0x1000,
@@ -114,4 +147,19 @@ fn main() {
     for inst in insts {
         println!("{:0X?} {:?}", inst.ip(), inst.display());
     }
+
+    let insts = [
+        Inst::Push(Reg::Rax),
+        Inst::Call(1768515945),
+        Inst::Pop(Reg::Rax),
+        Inst::Ret,
+    ];
+
+    let mut bytes = std::vec::Vec::<u8>::new();
+
+    for inst in insts {
+        bytes.extend_from_slice(inst.to_bytes().as_slice());
+    }
+
+    println!("{bytes:02X?}");
 }
